85caac9475c6ac85e4fce00c8397b41236f99d20
jswj30 <jswj30@naver.com>
2025-09-18 22:01:31 +0900

    Library screen 추가

--- navigations/BottomTabNavigation.tsx
+++ navigations/BottomTabNavigation.tsx
@@ -1,10 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/home/HomeScreen";
 import CalendarScreen from "../screens/calendar/CalendarScreen";
+import LibraryScreen from "../screens/library/LibraryScreen";
 
 export type BottomTabParamList = {
   Home: undefined;
   Calendar: undefined;
+  Library: undefined;
 };
 
 const Tab = createBottomTabNavigator();
@@ -18,6 +20,7 @@ export default function BottomTabNavigation() {
     >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Calendar" component={CalendarScreen} />
+      <Tab.Screen name="Library" component={LibraryScreen} />
     </Tab.Navigator>
   );
 }
