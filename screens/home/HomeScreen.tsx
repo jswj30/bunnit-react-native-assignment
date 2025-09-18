8bbabdafb2605b37003679329b67abe6fd27dafb
jswj30 <jswj30@naver.com>
2025-09-18 21:58:38 +0900

    Home - Text 수정

--- screens/home/HomeScreen.tsx
+++ screens/home/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <Text>Home</Text>
     </View>
   );
 }
