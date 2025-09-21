2c2c1499ee3e62bd34a9db65748be07cf0da831b
jswj30 <jswj30@naver.com>
2025-09-21 15:10:08 +0900

    react-native-gesture-handler 설치

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import RootNavigation from "./navigations/RootNavigation";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <RootNavigation />
-    </NavigationContainer>
+    <GestureHandlerRootView>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <RootNavigation />
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
